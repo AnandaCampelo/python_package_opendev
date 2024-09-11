f8ac42f636699f39b46f56a3098430d7ea6e8f00
AnandaCampelo <anandag.c@hotmail.com>
2024-09-11 14:18:32 -0300

    Localização e Tradução

--- scripts/hello.py
+++ scripts/hello.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 from dev_aberto.dev_aberto import hello
 
+import gettext
+gettext.bindtextdomain('cli', 'locale')
+gettext.textdomain('cli')
+_ = gettext.gettext
+
+# cli é o nome do arquivo em que guardamos nossas traduções
+# localedir é o caminho onde estão armazenadas as traduções. Pode ser um caminho relativo. 
+
 if __name__ == '__main__':
     date, name = hello()
-    print('Último commit feito em:', date, ' por', name)
+    print(_('Last commit made in:'), date, _('by'), name)
